79e9fbf6afad607ec7ee5069e7e7d5d473b538a2
Tilseam <tilseam@msn.com>
2025-02-15 12:44:44 +0800

    Update7.0.0: 调整carousel可以竖向拖动

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,71 +1,121 @@
 // Custom Scripts for Array Template //
 
-jQuery(function($) {
-    "use strict";
+jQuery(function ($) {
+  "use strict";
 
-        // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
-        var mainbottom = $('#main').offset().top;
+  // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+  var mainbottom = $('#main').offset().top;
 
-        // on scroll,
-        $(window).on('scroll',function(){
+  // on scroll,
+  $(window).on('scroll', function () {
 
-        // we round here to reduce a little workload
-        stop = Math.round($(window).scrollTop());
-        if (stop > mainbottom) {
-            $('.navbar').addClass('past-main');
-            $('.navbar').addClass('effect-main')
-        } else {
-            $('.navbar').removeClass('past-main');
-       }
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > mainbottom) {
+      $('.navbar').addClass('past-main');
+      $('.navbar').addClass('effect-main')
+    } else {
+      $('.navbar').removeClass('past-main');
+    }
 
-      });
+  });
 
 
   // Collapse navbar on click
 
-   $(document).on('click.nav','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') ) {
-    $(this).removeClass('in').addClass('collapse');
-   }
+  $(document).on('click.nav', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a')) {
+      $(this).removeClass('in').addClass('collapse');
+    }
   });
 
 
-    /*-----------------------------------
-    ----------- Scroll To Top -----------
-    ------------------------------------*/
+  /*-----------------------------------
+  ----------- Scroll To Top -----------
+  ------------------------------------*/
 
-    $(window).on('scroll', function () {
-      if ($(this).scrollTop() > 1000) {
-          $('#back-top').fadeIn();
-      } else {
-          $('#back-top').fadeOut();
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 1000) {
+      $('#back-top').fadeIn();
+    } else {
+      $('#back-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-top').on('click', function () {
+    $('#back-top').tooltip('hide');
+    $('body,html').animate({
+      scrollTop: 0
+    }, 1500);
+    return false;
+  });
+
+
+  /*-------- Owl Carousel ---------- */
+
+  $(".review-cards").owlCarousel({
+    slideSpeed: 200,
+    items: 1,
+    singleItem: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    pagination: false,
+    touchDrag: true,
+    mouseDrag: true,
+    smartSpeed: 300
+  });
+
+  // 处理轮播图触摸事件
+  $(".review-cards").each(function () {
+    let startY = 0;
+    let startX = 0;
+    let timestamp = 0;
+    let isScrolling = false;
+
+    const carousel = $(this)[0];
+
+    carousel.addEventListener('touchstart', function (e) {
+      startY = e.touches[0].pageY;
+      startX = e.touches[0].pageX;
+      timestamp = Date.now();
+      isScrolling = undefined;
+    }, { passive: true });
+
+    carousel.addEventListener('touchmove', function (e) {
+      if (!startX || !startY) {
+        return;
       }
-    });
-    // scroll body to 0px on click
-    $('#back-top').on('click', function () {
-      $('#back-top').tooltip('hide');
-      $('body,html').animate({
-          scrollTop: 0
-      }, 1500);
-      return false;
-    });
 
+      const deltaX = e.touches[0].pageX - startX;
+      const deltaY = e.touches[0].pageY - startY;
 
-    /*-------- Owl Carousel ---------- */
+      // 检测是否是垂直滚动
+      if (typeof isScrolling === 'undefined') {
+        isScrolling = Math.abs(deltaY) > Math.abs(deltaX);
+      }
 
-      $(".review-cards").owlCarousel({
-        slideSpeed: 200,
-        items: 1,
-        singleItem: true,
-        autoplay:true,
-        autoplayTimeout:2000,
-        autoplayHoverPause:true,
-        pagination: false,
-      });
+      // 如果是垂直滚动，不阻止默认行为
+      if (isScrolling) {
+        return;
+      }
+
+      // 只有在水平滑动足够距离时才阻止默认行为
+      if (Math.abs(deltaX) > 10 && !isScrolling) {
+        e.preventDefault();
+      }
+    }, { passive: false });
+
+    carousel.addEventListener('touchend', function () {
+      startX = 0;
+      startY = 0;
+      isScrolling = undefined;
+    }, { passive: true });
+  });
 
 
   /* ------ jQuery for Easing min -- */
-  (function($) {
+  (function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
@@ -83,7 +133,7 @@ jQuery(function($) {
     });
 
     // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').on('click', function() {
+    $('.js-scroll-trigger').on('click', function () {
       $('.navbar-collapse').collapse('hide');
     });
 
@@ -96,47 +146,48 @@ jQuery(function($) {
   })(jQuery); // End of use strict
 
 
-/* --------- Wow Init ------ */
+  /* --------- Wow Init ------ */
 
   new WOW().init();
 
 
   /* ----- Counter Up ----- */
 
-$('.counter').counterUp({
-		delay: 10,
-		time: 1000
-});
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+  });
 
-/*----- Preloader ----- */
+  /*----- Preloader ----- */
 
-    $(window).on('load', function() {
-  		setTimeout(function() {
-        $('#loading').fadeOut('slow', function() {
+  $(window).on('load', function () {
+    setTimeout(function () {
+      $('#loading').fadeOut('slow', function () {
+      });
+    }, 3000);
+  });
+
+
+  /*----- Subscription Form ----- */
+
+  $(document).ready(function () {
+    // jQuery Validation
+    $("#chimp-form").validate({
+      // if valid, post data via AJAX
+      submitHandler: function (form) {
+        $.post("assets/php/subscribe.php", { email: $("#chimp-email").val() }, function (data) {
+          $('#response').html(data);
         });
-      }, 3000);
+      },
+      // all fields are required
+      rules: {
+        email: {
+          required: true,
+          email: true
+        }
+      }
     });
-
-
-/*----- Subscription Form ----- */
-
-$(document).ready(function() {
-     // jQuery Validation
-     $("#chimp-form").validate({
-         // if valid, post data via AJAX
-         submitHandler: function(form) {
-             $.post("assets/php/subscribe.php", { email: $("#chimp-email").val() }, function(data) {
-                 $('#response').html(data);
-             });
-         },
-         // all fields are required
-         rules: {
-             email: {
-                 required: true,
-                 email: true
-             }
-         }
-     });
- });
+  });
 
 });
+
